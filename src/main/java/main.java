3d0d93b646e0a1723df23b52bc60a1098d195d17
emmanuel-rosillo <fb.verify.com@gmail.com>
2022-03-25 19:34:25 -0600

    Se añade graph a main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,14 @@
 public class main {
     public static void main(String[] args) {
-
+            int V =57;
+            Graph graph = new Graph(V);
+            graph.addEdge(0, 1);
+            graph.addEdge(0, 4);
+            graph.addEdge(1, 2);
+            graph.addEdge(1, 3);
+            graph.addEdge(1, 1);
+            graph.addEdge(2, 3);
+            graph.addEdge(3, 4);
+            graph.printGraph (graph) ;
     }
 }
